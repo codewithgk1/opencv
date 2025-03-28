1f1b130c821787a8a8ab7e68091ecc5471c380d0
mharkhani <mukesh.harkhani@gmail.com>
2025-03-28 17:49:30 +0530

    Add commented code examples for both image and video reading

--- read.py
+++ read.py
@@ -1,9 +1,18 @@
 import cv2 as cv
 
-img = cv.imread('Resources/Photos/cat_large.jpg')
+## Reading Images
+# img = cv.imread('Resources/Photos/cat_large.jpg')
+# cv.imshow('Cat',img) 
+# cv.waitKey(0)
+# cv.destroyAllWindows()
 
-cv.imshow('Cat',img) 
+## Reading Videos
+# capture = cv.VideoCapture('Resources/Videos/dog.mp4')
+# while True:
+#     isTrue, frame = capture.read()
+#     cv.imshow('Video',frame)
+#     if cv.waitKey(20) & 0xFF == ord('d'):
+#         break
 
-cv.waitKey(0)
-
-cv.destroyAllWindows()
+# capture.release()
+# cv.destroyAllWindows()
